a598ea7f52d1190842ddd22666a5111026acf22b
Cedar Taulbee <cedarthetree@gmail.com>
2019-11-25 14:22:07 -0800

    in class monday

--- myscripts.js
+++ myscripts.js
@@ -14,3 +14,32 @@ window.onclick = function(event) {
     }
   }
 }
+
+// your script file for adding your own jquery
+$(function() {
+// Your Code from here on down. Don't delete that line above!
+  
+
+$('.clickme').click(function(){
+    $('audio#orangutan')[0].play()
+
+})
+
+$('.pauseme').click(function(){
+  $('audio#orangutan')[0].pause()
+})
+
+$('.restartme').click(function(){
+  $('audio#orangutan')[0].currentTime = 0
+  $('audio#orangutan')[0].play()
+})
+
+
+
+
+
+
+
+
+// End of Your Code . Don't delete that line below!!
+});
