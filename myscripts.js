c95484cf5700a5d2fbee26a48ebcb387ac644336
Cedar Taulbee <cedarthetree@gmail.com>
2019-12-02 13:14:26 -0800

    leaves

--- myscripts.js
+++ myscripts.js
@@ -15,24 +15,6 @@ window.onclick = function(event) {
   }
 }
 
-// your script file for adding your own jquery
-$(function() {
-// Your Code from here on down. Don't delete that line above!
-  
-
-$('.clickme').click(function(){
-    $('audio#orangutan')[0].play()
-
-})
-
-$('.pauseme').click(function(){
-  $('audio#orangutan')[0].pause()
-})
-
-$('.restartme').click(function(){
-  $('audio#orangutan')[0].currentTime = 0
-  $('audio#orangutan')[0].play()
-})
 
 
 
@@ -41,5 +23,4 @@ $('.restartme').click(function(){
 
 
 
-// End of Your Code . Don't delete that line below!!
-});
+
